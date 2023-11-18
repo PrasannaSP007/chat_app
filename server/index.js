f699b8f8f6925d73d2cf7d1ffb3f60f40f46aa03
Prasanna S <131226651+PrasannaSP007@users.noreply.github.com>
2023-11-18 18:18:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors : {
         origin: ["https://chat-app-client-neon-eight.vercel.app/"],
-        methods: ["GET", "POST"]
+        methods: ["GET", "POST"],
+        credentials : true
     }
 });
 
