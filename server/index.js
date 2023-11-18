50f2887ac887a66b2481543214a896dcb0d38175
Prasanna S <131226651+PrasannaSP007@users.noreply.github.com>
2023-11-18 17:15:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors : {
         origin: ["http://localhost:3000", "https://chat-app-server-theta-vert.vercel.app/"],
-        methods: ["GET", "POST"]
+        methods: ["GET", "POST"],
+        credentials : true
     }
 });
 
