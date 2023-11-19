6261a466931d983d03932aeee4372eeabe685b61
Prasanna S <131226651+PrasannaSP007@users.noreply.github.com>
2023-11-19 16:00:14 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import io from 'socket.io-client';
 import Chat from './Chat';
 
-const socket = io.connect(process.env.REACT_APP_SERVER_URI);
+const socket = io.connect();
 
 function App() {
 
